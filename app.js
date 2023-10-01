11e147e8dfc1fefdc4f2de625a3c8e7b32b00fa0
Tsalsabillanf <tsalsabillanf2009994@gmail.com>
2023-10-02 00:55:10 +0700

    delete mahasiswa

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 // file entrypoint, dimana nanti API bakal jalan
 const express = require('express');
 const cors = require('cors');
-const { mahasiswaRoute } = require('./routes/mahasiswa.routes')
 const { leaderboardRoute } = require('./routes/index.routes')
 
 const app = express();
@@ -12,9 +11,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// rute untuk mahasiswa
-// rute untuk campus
-app.use("/mahasiswa", mahasiswaRoute)
+// rute untuk leaderboard
 app.use("/leaderboard", leaderboardRoute)
 
 app.listen(PORT, () => {
--- config/database.js
+++ config/database.js
@@ -1,12 +1,6 @@
 const mysql = require ('mysql2/promise');
 
 const pool = mysql.createPool({
-    // host: process.env.DB_HOST || "localhost",
-    // user: process.env.DB_USER || "root",
-    // password: process.env.DB_PASS || "Tsalsabillanf12",
-    // database: process.env.DB_NAME || "express_mysql",
-
-    // DB_URI=mysql://root:@localhost:3306/express_mysql
     uri: process.env.DB_URI,
 })
 
