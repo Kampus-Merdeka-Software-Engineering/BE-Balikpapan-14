0276bfd3cc351b9829ed717ef66d3b84c8c6bd07
Risma Ayu Prastika <rismaayuprastika03@gmail.com>
2023-10-03 00:22:57 +0700

    perubahan2

--- config/schema.sql
+++ config/schema.sql
@@ -2,6 +2,12 @@ CREATE DATABASE IF NOT EXISTS `terraceofdream`;
 
 USE `terraceofdream`;
 
+CREATE TABLE IF NOT EXISTS leaderboard (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(255) NOT NULL,
+    nilai INT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS lessons (
     id INT AUTO_INCREMENT PRIMARY KEY,
     image LONGBLOB NOT NULL,
@@ -16,31 +22,11 @@ CREATE TABLE IF NOT EXISTS profile (
     email VARCHAR(300) NOT NULL
 );
 
+INSERT INTO leaderboard (nama, nilai) VALUES ('salz', '90');
+
 INSERT INTO lessons (image, module) VALUES ('../../back-end-capstone-project-section-balikpapan-group-14/img/angklung.png', "https://drive.google.com/file/d/1mfh6t1jM6eRlCGZYfhIefEKVImq_upaT/view?usp=sharing");
 
 INSERT INTO profile (full_name, nisn, birthdate, email) VALUES ('Tsalsabilla Nurfitriyatna putri', '2009994', '2001-12-12', 'tsalsabilla@gmail.com');
 SELECT * FROM lessons;
 
 SELECT*FROM profile;
-CREATE DATABASE IF NOT EXISTS `express_mysql`;
-
-USE `express_mysql`;
-
-CREATE TABLE IF NOT EXISTS leaderboard (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR(255) NOT NULL,
-    nilai INT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS lessons (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    img VARCHAR(255) NOT NULL,
-    nama VARCHAR(255) NOT NULL
-);
-
--- ===================================
-INSERT INTO leaderboard (nama, nilai) VALUES
-('salz', '90');
-
-INSERT INTO lessons (img, nama) VALUES
-('https://ibb.co/4WfY0HM', 'social');
