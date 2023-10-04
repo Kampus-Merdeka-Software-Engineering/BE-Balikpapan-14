5e1b854bf103abe72ea7a6e09e8e898df4ecb41f
Risma Ayu Prastika <rismaayuprastika03@gmail.com>
2023-10-04 21:13:21 +0700

    ass1

--- services/assignment1.services.js
+++ services/assignment1.services.js
@@ -3,7 +3,7 @@ const {pool}= require('../config/database');
 const getAllAssignmentSatu = async () => {
     const connection = await pool.getConnection()
     try {
-        const [assignmentSatu]= await connection.query('SELECT*FROM Assignment1');
+        const [assignmentSatu]= await connection.query('SELECT*FROM assignment1');
         return assignmentSatu
     }catch (error) {
         console.log(error);
@@ -16,7 +16,7 @@ const getAllAssignmentSatu = async () => {
 const createAssignmentSatu = async (assignmentSatu) => {
     const connection = await pool.getConnection()
     try {
-        const createdAssignmentSatu= await connection.query('INSERT INTO Assignment1(assignment, deadline, date_modified, grade) VALUES (?, ?, ?, ?)',
+        const createdAssignmentSatu= await connection.query('INSERT INTO assignment1(assignment, deadline, date_modified, grade) VALUES (?, ?, ?, ?)',
         [assignmentSatu.assignment, assignmentSatu.deadline, assignmentSatu.date_modified, assignmentSatu.grade]);
         return createdAssignmentSatu
     } catch (error) {
@@ -29,7 +29,7 @@ const createAssignmentSatu = async (assignmentSatu) => {
 const getAssignmentSatuById = async (id) => {
     const connection = await pool.getConnection()
     try {
-        const [assignmentSatu] = await connection.query ('SELECT*FROM Assignment1 WHERE id= ?', [id]);
+        const [assignmentSatu] = await connection.query ('SELECT*FROM assignment1 WHERE id= ?', [id]);
         return assignmentSatu
     } catch (error) {
         return error
